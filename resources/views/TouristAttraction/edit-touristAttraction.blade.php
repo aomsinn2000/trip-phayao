4eaafc5d760e4ff53853ef0707507e692282f6da
aomsinn2000 <aomseven21@gmail.com>
2023-01-31 10:53:57 +0700

    สถานที่ยอดฮิตเพิ่มถังขยะ

--- resources/views/TouristAttraction/edit-touristAttraction.blade.php
+++ resources/views/TouristAttraction/edit-touristAttraction.blade.php
@@ -408,11 +408,17 @@
 
 
                         <div class="card card-body">
-                            <div class="row justify-content-end">
-                                <a href="{{ url('viewTouristAttraction') }}" type="button" class="btn btn-secondary" style="width:150px; ">ยกเลิก</a>
-                                <span class="px-2">
-                                    <a href="" type="button" class="btn btn-info" style="width:150px;">บันทึก</a>
-                                </span>
+                            <div class="row">
+                                <div class="col-2" style="float: left;">
+                                    <button type="button"  data-toggle="modal" data-target="#trashAll" style="border-radius:50%; height:40px;border-color:#ffff;" class="dropify-clear"><img src="{{ asset('/assets/images/icon/ion_trash-bin.png') }}" width="25"></button>
+                                </div>
+                                <div class="col-9">
+                                    <a href="{{ url('viewTouristAttraction') }}" type="button" class="btn btn-secondary" style="width:150px; ">ยกเลิก</a>
+                                    <span class="px-2">
+                                        <a href="" type="button" class="btn btn-info" style="width:150px;">บันทึก</a>
+                                    </span>
+                                </div>
+
                             </div>
                         </div>
 
@@ -421,6 +427,26 @@
 
 
                     </div>
+                      <!-- Modal ถังขยะ -->
+                      <div class="modal fade" id="trashAll" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-body text-center">
+                                    <br>
+                                    <h3 style="color: red;"> คุณแน่ใจใช่ไหมว่าต้องการลบข้อมูลนี้!</h3>
+                                    <svg style="color: rgb(206 16 16)" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                                    </svg>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
+                                    <button type="button" class="btn btn-primary" style="background-color: rgb(206 16 16)">ฉันแน่ใจ</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end Modal ถังขยะ -->
                 </form>
 
 
